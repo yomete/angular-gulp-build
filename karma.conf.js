13e5f509eed1ddad1602db4361b1982065608e14
Yomi Eluwande <iamlese@yahoo.com>
2016-09-09 16:47:19 +0100

    using gulp build now

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,17 @@
-//jshint strict: false
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+	'use strict';
 
-    basePath: './app',
-
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
-    ],
-
-    autoWatch: true,
-
-    frameworks: ['jasmine'],
-
-    browsers: ['Chrome'],
-
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter'
-    ],
-
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
-  });
+	config.set({
+		basePath: '',
+/*        logLevel: 'config.LOG_DEBUG',*/
+		frameworks: ['jasmine'],
+		files: [
+            'dist/js/bundle*.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'test/*.js'
+		],
+		autoWatch: true,
+		singleRun: false,
+		browsers: ['PhantomJS']
+	});
 };
